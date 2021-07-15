c0fe6e54f9e267efe25ff81831d705ee13787708
sidd-iscoding <mailsiddharthbhuyan@gmail.com>
2021-07-15 12:44:03 +0530

    List all the issues

---
+++ src/IssueList.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import queryString from 'query-string';
+//const queryString = require('query-string');
+
+import IssueFilter from './IssueFilter.jsx';
+import IssueTable from './IssueTable.jsx';
+import IssueAdd from './IssueAdd.jsx';
+import graphQLFetch from './graphQLFetch.js';
+
+export default class IssueList extends React.Component {
+  constructor() {
+    super();
+    this.state = { issues: [] };
+    this.createIssue = this.createIssue.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: { search: prevSearch } } = prevProps;
+    const { location: { search } } = this.props;
+    if (prevSearch !== search) {
+      this.loadData();
+    }
+  }
+
+  async loadData() {
+    const parsed = queryString.parse(this.props.location.search);
+    const vars = {};
+    if (parsed.status) vars.status = parsed.status;
+    const query = `query issueList($status:StatusType) {
+      issueList(status:$status) {
+        id title status owner
+        created effort due
+      }
+    }`;
+
+    const data = await graphQLFetch(query,vars);
+    if (data) {
+      this.setState({ issues: data.issueList });
+    }
+  }
+
+  async createIssue(issue) {
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+      issueAdd(issue: $issue) {
+        id
+      }
+    }`;
+
+    const data = await graphQLFetch(query, { issue });
+    if (data) {
+      this.loadData();
+    }
+  }
+
+  render() {
+    const { issues } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd createIssue={this.createIssue} />
+      </React.Fragment>
+    );
+  }
+}
